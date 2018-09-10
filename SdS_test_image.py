511579ded51f18229b1e66fa65ba5d2461fcd186
RomanCast <43135864+RomanCast@users.noreply.github.com>
2018-09-10 14:31:49 +0200

    meme chose mais correct

--- SdS_test_image.py
+++ SdS_test_image.py
@@ -29,7 +29,7 @@ print('je reconstruis les images de melanges......')
 xx1 = (x1 - np.min(x1)) / (np.max(x1) - np.min(x1)) * 255
 xx2 = (x2 - np.min(x2)) / (np.max(x2) - np.min(x2)) * 255
 
-image_mel1, image_mel2 = recons_images_test_nb(xx1, xx2, nb_lign, nb_col)
+image_mel1, image_mel2 = ri.recons_images_test_nb(xx1, xx2, nb_lign, nb_col)
 
 plt.figure(1)
 io.imshow(image_source1)
@@ -85,7 +85,7 @@ indice = 1 # compteur d'affichage
 
 
 for i in range(1,nb_iter):
-    DJ = compute_gradient(B,y1,y2,x1,x2,lambda0)
+    DJ = gr.compute_gradient(B,y1,y2,x1,x2,lambda0)
 
     B=B-mu*DJ # mise a jour de la matrice de separation
 
@@ -101,7 +101,7 @@ for i in range(1,nb_iter):
         print('je reconstruis les images separees......')
         yy1=(y1-np.min(y1))/(np.max(y1)-np.min(y1))*255
         yy2=(y2-np.min(y2))/(np.max(y2)-np.min(y2))*255
-        image_sep1, image_sep2 = recons_images_test_nb(yy1,yy2,nb_lign,nb_col)
+        image_sep1, image_sep2 = ri.recons_images_test_nb(yy1,yy2,nb_lign,nb_col)
         plt.figure(5)
         io.imshow(image_sep1)
         plt.title('sep1')
@@ -115,10 +115,10 @@ for i in range(1,nb_iter):
         #gray
         plt.show() # remplace le drawnow (normalement)
 
-        [Mat_or_cor_source] = correl_coef_composante_nb(s1,s2) # Calcul de la correlation entre les sources avant melange
+        [Mat_or_cor_source] = cc.correl_coef_composante_nb(s1,s2) # Calcul de la correlation entre les sources avant melange
         plt.pause(1)
 
-        [Mat_mel_cor] = correl_coef_composante_nb(x1,x2) # Calcul de la correlation entre les sources melangees
+        [Mat_mel_cor] = cc.correl_coef_composante_nb(x1,x2) # Calcul de la correlation entre les sources melangees
 
-        [Mat_sep_cor] = correl_coef_composante_nb(y1,y2) # Calcul de la correlation entre les sources separees
+        [Mat_sep_cor] = cc.correl_coef_composante_nb(y1,y2) # Calcul de la correlation entre les sources separees
         plt.pause(5)
