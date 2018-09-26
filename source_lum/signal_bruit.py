021d019999b1a4bf79f7c22542914be980c1b089
RomanCast <43135864+RomanCast@users.noreply.github.com>
2018-09-26 13:55:43 +0200

    un peu mieux

--- source_lum/signal_bruit.py
+++ source_lum/signal_bruit.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def rapp_signal_bruit(B, A):
+def rapp_signal_bruit(B, A, y):
     residu = np.dot(np.dot(B,A),S)
     SNR = 10*np.log(np.mean(y**2)/np.mean(residu**2))
     return(SNR)
